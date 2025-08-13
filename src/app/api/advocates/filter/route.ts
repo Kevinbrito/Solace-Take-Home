4cce5d5d8a26a2cef7f85d66d025d3ebe25239f1
KevinBrito <britkev94@gmail.com>
2025-08-13 14:22:41 -0400

    create the mvp

--- src/app/api/advocates/filter/route.ts
+++ src/app/api/advocates/filter/route.ts
@@ -1,5 +1,5 @@
-import db from "../../../db";
-import { advocates } from "../../../db/schema";
+import db from "@/db";
+import { advocates } from "@/db/schema";
 import { gte, ilike, sql } from "drizzle-orm";
 
 export async function GET(
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import { Advocate } from "./type";
 import RadioButtons from "./components/radio-buttons/radio-buttons";
+import AdvocateTable from "./components/table/advocate-table";
 
 export default function Home() {
   const [advocates, setAdvocates] = useState<Advocate[]>([]);
@@ -67,53 +68,9 @@ export default function Home() {
 
   return (
     <main style={{ margin: "24px" }}>
-      <h1>Solace Advocates</h1>
-      <br />
-      <br />
-      <div>
-        <p>Search</p>
-        <p>
-          Searching for: <span id="search-term"></span>
-        </p>
-        <input style={{ border: "1px solid black" }} onChange={onChange} />
-        
-        <button onClick={onClick}>Reset Search</button>
-        <span className="flex gap-1">Filter by: <RadioButtons handleChange={handleRadioButtonChange} selectedValue={selectedValue} /></span>
-      </div>
-      <br />
-      <br />
-      <table>
-        <thead>
-          <tr>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>City</th>
-          <th>Degree</th>
-          <th>Specialties</th>
-          <th>Years of Experience</th>
-          <th>Phone Number</th>
-          </tr>
-        </thead>
-        <tbody>
-          {advocates.map((advocate) => {
-            return (
-              <tr>
-                <td>{advocate.firstName}</td>
-                <td>{advocate.lastName}</td>
-                <td>{advocate.city}</td>
-                <td>{advocate.degree}</td>
-                <td>
-                  {advocate.specialties.map((s) => (
-                    <div>{s}</div>
-                  ))}
-                </td>
-                <td>{advocate.yearsOfExperience}</td>
-                <td>{advocate.phoneNumber}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <input id="search-term" className="border" onChange={onChange} />
+      <span className="flex gap-1">Filter by: <RadioButtons handleChange={handleRadioButtonChange} selectedValue={selectedValue} /></span>
+      <AdvocateTable filteredAdvocates={filteredAdvocates.length > 0 ? filteredAdvocates : advocates}/>
     </main>
   );
 }
